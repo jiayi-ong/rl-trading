8c1b54c8a0175df7da23fd291ff87d831eacceb8
James_Jia Yi ONG <james.ong.jiayi@gmail.com>
2023-07-22 23:18:20 -0400

    Added code to class TraderAgent

--- RL_Trading.py
+++ RL_Trading.py
@@ -1,4 +1,33 @@
+import numpy as np
+
+
+
 class TraderAgent:
     """
     """
-    pass
+
+    def __init__(self, stock, trader_algorithm):
+        """
+        Args:
+            stock (SimpleStock):
+
+            trader_algorithm (str):
+                either 'random' or 'Q-learning'
+        """
+        pass
+
+    
+    @staticmethod
+    def stable_softmax(x, axis=1):
+        """ Numerically stable softmax:
+        softmax(x) = e^x /(sum(e^x))
+                = e^x / (e^max(x) * sum(e^x/e^max(x)))
+        Args:
+            x: A 1-dimensional array of floats
+        Returns:
+            output: softmax(x)
+        """
+        z = np.exp(x - np.max(x))
+        output = z / np.sum(z)
+
+        return output
